772186c0ec7e4225e3bb6b7972c1b6a01f2d30bb
Truong vi <vinlt@antsprogrammatic.com>
2020-06-05 22:48:27 +0700

    hello world

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -10,6 +10,7 @@ export default function Post({ postData }) {
             <title>{postData.title}</title>
         </Head>
         <article>
+            <div>no thing</div>
             <h1 className={utilStyles.headingXl}>{postData.title}</h1>
             <div className={utilStyles.lightText}>
                 {moment(postData.date, 'YYYY-MM-DD').locale('vi').format('ll')}
